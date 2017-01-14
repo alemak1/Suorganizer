042a3f0064950dfd43658e633c62ec51176578ff
yongrenwang <czie.alexmak@gmail.com>
2017-01-14 15:10:37 +0800

    Defined models for organizer and blog apps

--- suorganizer/blog/models.py
+++ suorganizer/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from organizer.models import Startup,Tag
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=63)
+	slug = models.SlugField(max_length=63, help_text='A label for URL config.',unique_for_month='pub_date')
+	text = models.TextField()
+	pub_date = models.DateField('date published', auto_now_add=True)
+	tags = models.ManyToManyField(Tag,related_name='blog_posts')
+	startups = models.ManyToManyField(Startup,related_name='blog_posts')
+
