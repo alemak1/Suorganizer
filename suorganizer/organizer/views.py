bd99a6cfee3b87e0c6764cf9fd9af54685134b55
yongrenwang <czie.alexmak@gmail.com>
2017-01-15 19:17:55 +0800

    Replaced render_to_response() with the render() method

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Tag
 from django.http.response import HttpResponse,Http404
 from django.template import Context, loader
@@ -6,8 +6,8 @@ from django.template import Context, loader
 
 
 def homepage(request):
-	return render_to_response('organizer/tag_list.html',{'tag_list':Tag.objects.all()})
+	return render(request,'organizer/tag_list.html',{'tag_list':Tag.objects.all()})
 
 def tag_detail(request, slug):
 	tag = get_object_or_404(Tag,slug__iexact=slug)
-	return render_to_response('organizer/tag_detail.html',{'tag':tag})
+	return render(request,'organizer/tag_detail.html',{'tag':tag})
