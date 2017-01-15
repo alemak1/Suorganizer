8644cea8ec0f7f7642f9ad7e5823d42bc39b7930
yongrenwang <czie.alexmak@gmail.com>
2017-01-15 08:44:59 +0800

    Added a function view to organizer.views; also, populated the database with some data

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Tag
+from django.http.response import HttpResponse
 # Create your views here.
+
+
+def homepage(request):
+	tag_list = Tag.objects.all()
+	output = ", ".join([tag.name for tag in tag_list])
+	return HttpResponse(output)
