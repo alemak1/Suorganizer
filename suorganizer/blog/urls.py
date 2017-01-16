95645c1f9dcf7fee0a9e9c18e3c9baace2694175
yongrenwang <czie.alexmak@gmail.com>
2017-01-16 13:05:52 +0800

    Added named parameters to CBV get() method as well as the post_detail() function view

--- suorganizer/blog/urls.py
+++ suorganizer/blog/urls.py
@@ -4,9 +4,11 @@ from django.contrib import admin
 from .views import PostList, post_detail
 
 urlpatterns = [
-	url(r'^$',PostList.as_view(),name="blog_post_list"),
+	url(r'^$',PostList.as_view(),{'parent_template':'base.html'},name="blog_post_list"),
 	url(r'^(?P<year>\d{4})/'
 		r'(?P<month>\d{1,2})/'
 		r'(?P<slug>[\w\-]+)/$',
-		post_detail,name="blog_post_detail")
+		post_detail,
+		{'parent_template':'base.html'}},
+		name="blog_post_detail")
 ]
--- suorganizer/blog/views.py
+++ suorganizer/blog/views.py
@@ -6,13 +6,14 @@ from django.views.generic import View
 
 class PostList(View):
 
-	def get(self,request):
+	def get(self,request,parent_template=None):
 		return render(
 			request,
 			'blog/post_list.html',
-			{'post_list':Post.objects.all()})
+			{'post_list':Post.objects.all(),
+			'parent_template':parent_template})
 
-def post_detail(request):
+def post_detail(request,year,month,slug,parent_template=None):
 	post = get_object_or_404(
 		Post,
 		pub_date__year=year,
@@ -21,4 +22,5 @@ def post_detail(request):
 	return render(
 		request,
 		'blog/post_detail.html',
-		{'post':post})
+		{'post':post,
+		'parent_template':parent_template})
